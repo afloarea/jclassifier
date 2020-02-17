de5b5d8b24e7db8a569a24a5d6f558f46783e4fe
afloarea <adrianfloarea07@yahoo.com>
2020-02-17 22:17:28 +0200

    Add junit5 dependency and module-info and app entry point

---
+++ src/main/java/com/github/afloarea/jclassifier/Main.java
@@ -0,0 +1,9 @@
+package com.github.afloarea.jclassifier;
+
+public final class Main {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module com.github.afloarea.jclassifier {
+}
