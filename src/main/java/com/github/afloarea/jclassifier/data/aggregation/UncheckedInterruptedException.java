d54ce3a7cf093292a26d263d96ed102b5b5f4893
afloarea <adrianfloarea07@yahoo.com>
2020-02-20 00:07:16 +0200

    Add surefire plugin, specify compiler plugin version and junit version

---
+++ src/main/java/com/github/afloarea/jclassifier/data/aggregation/UncheckedInterruptedException.java
@@ -0,0 +1,4 @@
+package com.github.afloarea.jclassifier.data.aggregation;
+
+public class UncheckedInterruptedException {
+}
---
+++ src/main/java/com/github/afloarea/jclassifier/extractors/RgbToGrayExtractor.java
@@ -0,0 +1,4 @@
+package com.github.afloarea.jclassifier.extractors;
+
+public class RgbToGrayFeatureExtractor {
+}
