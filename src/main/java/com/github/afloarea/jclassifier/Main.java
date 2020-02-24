a0f7703de056727b642f8b3efcb220602cf2142a
afloarea <adrianfloarea07@yahoo.com>
2020-02-25 00:28:49 +0200

    Add main class

--- src/main/java/com/github/afloarea/jclassifier/Main.java
+++ src/main/java/com/github/afloarea/jclassifier/Main.java
@@ -1,9 +1,49 @@
 package com.github.afloarea.jclassifier;
 
-public final class Main {
+import com.github.afloarea.jclassifier.classifiers.knn.Knn;
+import com.github.afloarea.jclassifier.data.AggregatedData;
+import com.github.afloarea.jclassifier.data.DataSet;
+import com.github.afloarea.jclassifier.data.aggregation.DataAggregator;
+import com.github.afloarea.jclassifier.evaluators.BasicEvaluator;
+import com.github.afloarea.jclassifier.evaluators.Statistics;
+import com.github.afloarea.jclassifier.extractors.ExtractorType;
+import com.github.afloarea.jclassifier.extractors.FeatureExtractor;
 
-    public static void main(String[] args) {
-        //TODO
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        final DataAggregator aggregator = new DataAggregator.Builder()
+                .withRandom(new Random(12))
+                .withRootDataFolderPath("datasets/UCMerced_LandUse/Images")
+                .withTrainPercentage(0.7)
+                .withNormalized(false)
+                .withFeatureExtractor(FeatureExtractor.of(ExtractorType.HSV_CONCATENATED, 360, 256, 256))
+                .build();
+
+        LOGGER.info("Start collecting data...");
+        long ref = System.currentTimeMillis();
+        final AggregatedData aggregatedData = aggregator.aggregateData(true);
+        LOGGER.log(Level.INFO, "Finished reading data and extracting features: {0} seconds", (System.currentTimeMillis() - ref) / 1000);
+
+        final DataSet trainSet = aggregatedData.getTrainData();
+        final DataSet testSet = aggregatedData.getTestData();
+
+        final Knn knn = Knn.of(trainSet.getFeatures(), trainSet.getLabels(), 7);
+        LOGGER.info("Start classification...");
+        ref = System.currentTimeMillis();
+        final int[] guessedLabels = knn.classify(testSet.getFeatures());
+        LOGGER.log(Level.INFO, "Finished classifying data: {0} seconds", (System.currentTimeMillis() - ref) / 1000);
+
+        final Statistics stats = BasicEvaluator.evaluate(testSet.getLabels(), guessedLabels);
+
+        System.out.println(stats);
+        System.out.println(aggregatedData.getLabelsMap());
     }
 
 }
